9647bea6841a689665d31d16c31b33dd42bcba9b
eliasdarruda <eliasdarruda@gmail.com>
2022-07-07 02:57:29 -0300

    export useful stuff for client

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import 'reflect-metadata';
 
 import SpawnSystem from '@spawn/spawn_system'
 
+export { ActorEntity, Command } from '@spawn/decorators/actor'
+export * from '@spawn/integration/errors'
+export { ActorContext, Value } from '@spawn/client_actor/actor_context'
+export { ActorOpts } from '@spawn/client_actor/actor_opts'
+
 export { SpawnSystem }
